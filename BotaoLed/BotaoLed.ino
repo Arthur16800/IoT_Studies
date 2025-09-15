97fbbcabc3449b26a0ca2b722bed106ebe9a31b9
Arthur16800 <arthur08coutinho@gmail.com>
2025-09-15 12:40:51 -0300

    Armando alarme com botão 15/09

--- BotaoLed/BotaoLed.ino
+++ BotaoLed/BotaoLed.ino
@@ -1,5 +1,5 @@
 #include <math.h>
-// #include "AdafruitIO_WiFi.h"
+#include "AdafruitIO_WiFi.h"
 #include "NewPing.h"
 
 #define BUZZER_PIN 27
@@ -12,25 +12,23 @@
 #define MAX_DISTANCE 100
 NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
 
-
-
-
-
 // // Configurações da rede WIFI
-// #define WIFI_SSID "IIIII"
-// #define WIFI_PASS "bonas231"
+#define WIFI_SSID "IIIII"
+#define WIFI_PASS "bonas231"
 
 // // Autenticação Adafruit IO
-// #define IO_USERNAME "Arthur_07"
-// #define IO_KEY ""
+#define IO_USERNAME "Arthur_07"
+#define IO_KEY ""
 
-
-
-// AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
 // #define pinLed 14 //Pino do LED
 
-// AdafruitIO_Feed *botaoled = io.feed("botaoled");
+AdafruitIO_Feed *botaoalarme = io.feed("botaoalarme");
+
+bool alarmeAtivo = false;
+unsigned int distancia = 0;
+int LIMITE_DISTANCIA = 15;
 
 void setup() {
 
@@ -42,30 +40,41 @@ void setup() {
 
   Serial.begin(115200);
 
-  while(!Serial);
+  while (!Serial)
+    ;
 
   // Serial.print("Conectando ao Adafruit IO");
-  // io.connect();
+  io.connect();
 
-  // while(io.status() < AIO_CONNECTED){
-  //   Serial.print(".");
-  //   delay(500);
-  // }
+  while (io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("Adafruit Conectado");
+
+  botaoalarme->onMessage(handleAlarme);
+
+  Serial.println("Solicitando o estado inicial do alarme: ");
+  botaoalarme->get();
+
+
 
   // Serial.println();
   // Serial.println(io.statusText());
 
   // // Configuração do callack, quando o feed receber(atualizar) um valor
   // botaoled -> onMessage(handleBotaoLed);
+
   // // Registra a função de callback
   // // Ela será chamada sempre que o feed receber um novo dado
-
 }
 
 void loop() {
 
   // Manter a conexão com o Adafruit IO ativa
-  // io.run();
+  io.run();
   // testeBuzzer();
   // testeLed();
 
@@ -73,6 +82,28 @@ void loop() {
   Serial.print(F("Distancia lida: "));
   Serial.println(sonar.ping_cm());
 
-  delay(500);
+  // Leitura botao fisico
+  if (digitalRead(BOTAO_FISICO) == 1) {
+    delay(200);
+    alarmeAtivo = !alarmeAtivo;
 
+    botaoalarme->save(alarmeAtivo ? "true" : "false");
+    Serial.println(alarmeAtivo ? "Alarme ARMADO pelo botao fisico" : "Alarme DESARMADO pelo botao fisico");
+  }
+
+
+  distancia = sonar.ping_cm();
+  Serial.print("Distancia lida: ");
+  Serial.println(distancia);
+  Serial.print(" cm");
+
+  // ativação ou desativação do alarme
+  if(alarmeAtivo && distancia > 0 && distancia < LIMITE_DISTANCIA){
+    ativarAlerta();
+  }
+  else{
+    desativarAlerta();
+  }
+
+  delay(500);
 }
